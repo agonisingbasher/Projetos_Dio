c5deedd44ff0a8ccf139a5bfb346a56c19e3ff61
AgonisingBasher <felipealexandre10assis@gmail.com>
2026-09-25 00:02:36 -0300

    Add string concatenation script in concatenacao.py

---
+++ Projeto-3/Dados/concatenacao.py
@@ -0,0 +1,8 @@
+# Vamos receber 2 valores diferentes do usuário e concatená-los em uma única string
+info1 = input("Digite a primeira informação: ")
+info2 = input("Digite a segunda informação: ")
+
+# Concatenando os valores com um espaço no meio
+resultado = info1 + " " + info2
+
+print("Resultado da concatenação:", resultado)
